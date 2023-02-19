43a559bc81d74c58913e643ca652e9c36edc2582
minhqdao <asiancorporator@yahoo.de>
2023-02-19 21:27:53 +0100

    Put back and test negative case for format

--- test/check.f90
+++ test/check.f90
@@ -7,8 +7,8 @@ program check
 
 !################################### Create ###################################!
 
-  v1 = version_t(0, 0, 0)
-  if (v1%to_string() /= '0.0.') then
+    v1 = version_t(0, 0, 0)
+  if (v1%to_string() /= '0.0.0') then
     call fail("Parsing failed for '"//v1%to_string()//"'")
   end if
 
