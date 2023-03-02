ff90a54837796052fac8530dd50d68fcd875bf5f
minhqdao <asiancorporator@yahoo.de>
2023-03-02 02:22:53 +0100

    Revert, check another test

--- src/version_f.f90
+++ src/version_f.f90
@@ -236,12 +236,10 @@ contains
   pure subroutine increment_prerelease(this)
     class(version_t), intent(inout) :: this
 
-    integer :: number
-
     if (allocated(this%prerelease)) then
       if (this%prerelease(size(this%prerelease))%is_numeric()) then
-        number = this%prerelease(size(this%prerelease))%num() + 1
-        this%prerelease(size(this%prerelease))%str = trim(int2s(number))
+        this%prerelease(size(this%prerelease))%str = &
+        & trim(int2s(this%prerelease(size(this%prerelease))%num() + 1))
       else
         this%prerelease = [this%prerelease, string_t('1')]
       end if
