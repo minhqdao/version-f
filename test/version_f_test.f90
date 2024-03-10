ae2bda6efe1e480cfb3df4de5d9c6f850a440e4a
minhqdao <hello@minhdao.de>
2024-03-11 00:28:08 +0545

    Simplify test

--- test/version_f_test.f90
+++ test/version_f_test.f90
@@ -3,14 +3,4 @@ program test
   implicit none
 
   call try_satisfy()
-
-  print *, achar(10)//achar(27)//'[92m All tests passed.'//achar(27)
-
-contains
-
-  subroutine fail(msg)
-    character(*), intent(in) :: msg
-    print *, achar(27)//'[31m'//'Test failed: '//msg//achar(27)//'[0m'
-    stop 1
-  end
 end program
